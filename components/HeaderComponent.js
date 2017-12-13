35df42fe388cb403b051ac417855016f8cbc518e
selvaganeshrajam@gmail.com <selvaganeshrajam@gmail.com>
2017-12-13 15:51:58 +0530

    header component

---
+++ components/HeaderComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, Text, View,ToolbarAndroid } from 'react-native';
+
+export default class HeaderComponent extends React.Component {
+  render() {
+    return (
+      <View >
+        <Text>Welcome to survey app!Please sign in</Text>
+      </View>
+    );
+  }
+}
+
